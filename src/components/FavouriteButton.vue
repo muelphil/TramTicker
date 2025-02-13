7657149f05e88584a570673a634421a75e7ea6a7
Philip <muellers.philip@googlemail.com>
2025-02-13 13:04:21 +0100

    'Deploy'

---
+++ src/components/FavouriteButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Settings</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// defineProps<{
+//   stationId: number
+// }>()
+</script>
+
+<style scoped>
+</style>
--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const BUILD_TIMESTAMP = '2025-02-13T11:37:44.570Z';
+export const BUILD_TIMESTAMP = '2025-02-13T12:04:18.169Z';
 export default {}
